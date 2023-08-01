e11d2e50a7c23e181f171094edabafd49de775fa
EeyoreLee <li.chunyu0412@gmail.com>
2023-08-01 18:29:33 +0800

    update

--- src/train.py
+++ src/train.py
@@ -7,12 +7,14 @@ import argparse
 from dataclasses import dataclass, field
 
 import deepspeed
+import numpy as np
 import torch
 import torch.nn.functional as F
 from peft import LoraConfig, TaskType, get_peft_model
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.dataset import T_co
+from torch.utils.data.distributed import DistributedSampler
 from transformers import (
     AutoModel,
     AutoTokenizer,
@@ -21,6 +23,7 @@ from transformers import (
 )
 
 from utils.data import get_data
+from utils.generic import get_eval_metrics, get_gen_kwargs
 
 
 class CausalDataset(Dataset):
@@ -28,13 +31,13 @@ class CausalDataset(Dataset):
     def __init__(
         self,
         file_path: str,
-        tokenizer_name_or_path: str,
+        tokenizer: PreTrainedTokenizerBase,
         max_q_length=100,
         max_a_length=50,
     ) -> None:
         super().__init__()
         self.data = get_data(file_path=file_path)
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path, trust_remote_code=True, revision="main")
+        self.tokenizer = tokenizer
         self.max_q_length = max_q_length
         self.max_a_length = max_a_length
         self.max_seq_length = max_q_length + max_a_length + 1
@@ -55,6 +58,36 @@ class CausalDataset(Dataset):
         labels = [(i if i != self.tokenizer.pad_token_id else -100) for i in labels]
 
         return {"input_ids": input_ids, "labels": labels}
+
+    def __len__(self):
+        return len(self.data)
+
+
+class GenerationDataset(Dataset):
+
+    def __init__(
+        self,
+        file_path: str,
+        tokenizer: PreTrainedTokenizerBase,
+        max_q_length=100,
+        max_a_length=50,
+    ) -> None:
+        self.data = get_data(file_path=file_path)
+        self.tokenizer = tokenizer
+        self.max_q_length = max_q_length
+        self.max_a_length = max_a_length
+        self.max_seq_length = max_q_length + max_a_length + 1
+
+    def __getitem__(self, index):
+        q = self.data[index]["q"]
+        a = self.data[index]["a"]
+        history = self.data[index]["history"]
+        prompt = self.tokenizer.build_prompt(q, history)
+        inputs = self.tokenizer(prompt, max_length=self.max_q_length, truncation=True, padding=True)
+        labels = self.tokenizer(text_target=a, max_length=self.max_a_length, truncation=True)
+        labels["input_ids"] = [i if i != self.tokenizer.pad_token_id else -100 for i in labels["input_ids"]]
+        inputs["labels"] = labels["input_ids"]
+        return inputs
 
     def __len__(self):
         return len(self.data)
@@ -90,6 +123,7 @@ def main():
     file_path = "../data/data_example.jsonl"
     max_q_length = 100
     max_a_length = 500
+    max_length = max_q_length + max_a_length + 1
     batch_size = 1
     # learning_rate = 1e-4
     # accumulate_step = 1
@@ -97,43 +131,90 @@ def main():
     # num_warmup_steps = 0
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--deepspeed_config", type=str, help="deepspeed config")
-    parser.add_argument("--local_rank", type=int, help="")
+    parser.add_argument("--local_rank", type=int, default=-1, help="")
+    parser.add_argument("--num_beams", type=int, default=1, help="number of beam search")
+    parser.add_argument("--synced_gpus", type=bool, default=False, help="True if zero3 enabled")
+    parser.add_argument("--do_sample", type=bool, default=True, help="")
+    parser.add_argument("--top_p", type=float, default=0.7, help="")
+    parser.add_argument("--temperature", type=float, default=0.95, help="")
+    parser = deepspeed.add_config_arguments(parser=parser)
     args = parser.parse_args()
+    deepspeed.init_distributed()
+    args.global_rank = torch.distributed.get_rank()
 
     peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM,
                              inference_mode=False,
                              r=4,
-                             lora_alpha=32,
+                             lora_alpha=8,
                              lora_dropout=0.1,
                              target_modules=["query_key_value"],
                              fan_in_fan_out=False,
                              bias="all")
-
     model = AutoModel.from_pretrained(model_name_or_path, trust_remote_code=True, revision="main").half()
+    model.gradient_checkpointing_enable()
     model = get_peft_model(model, peft_config)
-    # model.print_trainable_parameters()
-    model_engine, optimizer, _, _ = deepspeed.initialize(args=args, model=model)
+    model.print_trainable_parameters()
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    model, optimizer, _, _ = deepspeed.initialize(args=args, model=model, model_parameters=parameters, dist_init_required=True)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path,
+                                              trust_remote_code=True,
+                                              revision="main",
+                                              padding_side="left")
     train_dataset = CausalDataset(file_path=file_path,
-                                  tokenizer_name_or_path=tokenizer_name_or_path,
+                                  tokenizer=tokenizer,
                                   max_q_length=max_q_length,
                                   max_a_length=max_a_length)
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path, trust_remote_code=True, revision="main")
+    train_sampler = DistributedSampler(train_dataset)
     train_dataloader = DataLoader(train_dataset,
                                   batch_size=batch_size,
+                                  sampler=train_sampler,
                                   collate_fn=DataCollatorForSeq2Seq(tokenizer=tokenizer,
                                                                     model=model,
                                                                     label_pad_token_id=-100,
                                                                     pad_to_multiple_of=None,
                                                                     padding=True))
 
-    for step, batch in enumerate(train_dataloader):
-        batch = {k: v.cuda() for k, v in batch.items()}
-        output = model_engine(**batch)
-        loss = output.loss
-        model_engine.backward(loss)
-        model_engine.step()
-        print("step success")
+    eval_dataset = GenerationDataset(file_path=file_path,
+                                     tokenizer=tokenizer,
+                                     max_q_length=max_q_length,
+                                     max_a_length=max_a_length)
+    eval_sampler = DistributedSampler(eval_dataset, shuffle=False)
+    eval_dataloader = DataLoader(eval_dataset,
+                                 batch_size=batch_size,
+                                 sampler=eval_sampler,
+                                 collate_fn=DataCollatorForSeq2Seq(tokenizer=tokenizer,
+                                                                   model=model,
+                                                                   label_pad_token_id=-100,
+                                                                   pad_to_multiple_of=None,
+                                                                   padding=True))
+
+    for epoch in range(4):
+
+        model.train()
+        for step, batch in enumerate(train_dataloader):
+            batch = {k: v.to(model.local_rank) for k, v in batch.items()}
+            output = model(**batch)
+            loss = output.loss
+            model.backward(loss)
+            model.step()
+
+        model.eval()
+        eval_metrics = {'rouge-1': [], 'rouge-2': [], 'rouge-l': [], 'bleu@4': []}
+        for step, batch in enumerate(eval_dataloader):
+            batch = {k: v.to(model.local_rank) for k, v in batch.items()}
+            gen_kwargs = get_gen_kwargs(inputs=batch, args=args)
+            gen_kwargs["max_length"] = max_length
+            generated_tokens = model.generate(**gen_kwargs)
+            generated_tokens = generated_tokens[:, gen_kwargs["input_ids"].size()[-1]:]
+            generated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+            labels_text = tokenizer.batch_decode(batch["labels"], skip_special_tokens=True)
+            metrics = get_eval_metrics(generated_text, labels_text)
+            for k, v in metrics.items():
+                eval_metrics[k].append(v)
+        for k, v in eval_metrics.items():
+            eval_metrics[k] = np.mean(v)
+        print(eval_metrics)
+        ...
 
     print("success")
 
---
+++ src/utils/generic.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+'''
+@create_time: 2023/08/01 17:46:02
+@author: lichunyu
+'''
+import argparse
+from typing import Dict, List
+
+import jieba
+import numpy as np
+import torch
+from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
+from rouge_chinese import Rouge
+from transformers import PreTrainedTokenizer
+
+
+def get_gen_kwargs(inputs: dict, args: argparse.ArgumentParser) -> dict:
+    gen_kwargs = {}
+    gen_kwargs["num_beams"] = args.num_beams
+    gen_kwargs["synced_gpus"] = args.synced_gpus
+    gen_kwargs["input_ids"] = inputs["input_ids"]
+    gen_kwargs["attention_mask"] = inputs["attention_mask"]
+    gen_kwargs["position_ids"] = inputs["position_ids"]
+    gen_kwargs["top_p"] = args.top_p
+    gen_kwargs["do_sample"] = args.do_sample
+    gen_kwargs["temperature"] = args.temperature
+    return gen_kwargs
+
+
+def get_eval_metrics(source: List[str], target: List[str]) -> Dict[str, float]:
+    scores = {"rouge-1": [], "rouge-2": [], "rouge-l": [], "bleu@4": []}
+    for s, t in zip(source, target):
+        hypothesis = list(jieba.cut(s))
+        reference = list(jieba.cut(t))
+        rouge = Rouge()
+        result = rouge.get_scores(' '.join(hypothesis), ' '.join(reference))[0]
+        for k, v in result.items():
+            scores[k].append(v["f"])
+        bleu_score = sentence_bleu([list(t)], list(s), smoothing_function=SmoothingFunction().method3)
+        scores["bleu@4"].append(bleu_score)
+    for k, v in scores.items():
+        scores[k] = float(np.mean(v))
+    return scores
+
+
+def tensor_pad(tensor: torch.Tensor, tokenizer: PreTrainedTokenizer, max_length: int):
+    padded_tensor = tokenizer.pad_token_id * torch.ones((tensor.shape[0], max_length), dtype=tensor.dtype, device=tensor.device)
+    padded_tensor[:, :tensor.shape[-1]] = tensor
+    return padded_tensor
